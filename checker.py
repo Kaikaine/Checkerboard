8b565f1ae400f41990bbd9d5d0f71f29002f5bd6
Kairi <kairijb@gmail.com>
2018-04-03 15:03:36 -0500

    first commit

---
+++ checker.py
@@ -0,0 +1,10 @@
+def patterns(rows):
+    i=0
+    while i <= rows:
+        pattern = "* * * *\n * * * *"
+        print pattern
+        i+=1
+
+patterns(7)
+
+# Creates a checkerboard like pattern
